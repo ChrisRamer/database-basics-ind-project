904df3c5e0e98d1f8d2b206e8949d9b721c40e0c
Chris Ramer <chrisramer.97@gmail.com>
2021-04-09 11:22:50 -0700

    Add date of last visit property to client

--- HairSalon.Solution/HairSalon/Models/Client.cs
+++ HairSalon.Solution/HairSalon/Models/Client.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace HairSalon.Models
 {
 	public class Client
 	{
 		public int ClientId { get; set; }
 		public string Name { get; set; }
+		public DateTime DateOfLastVisit { get; set; }
 		public int StylistId { get; set; }
 		public virtual Stylist Stylist { get; set; }
 	}
